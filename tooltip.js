286b540293ba2f6da4d386a4c4d5a31a66edfd66
liutkevych <oli@keepit.com>
2018-12-24 14:12:43 +0200

    Use attributes in custom web component

--- tooltip.js
+++ tooltip.js
@@ -1,19 +1,31 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
-        console.log('The constructor is invoked!')
+        this._tooltipContainer;
+        this._tooltipText = 'Some hurdcoded tooltip text!';
     }
 
+    // Only in this place we can place our element in DOM
     connectedCallback() {
+        if (this.hasAttribute('text')){
+            this._tooltipText = this.getAttribute('text');
+        }
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?)';
-        tooltipIcon.addEventListener('mouseenter', this._showTooltip);
+        tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+        tooltipIcon.addEventListener('mouseleave', this._destroyTooltip.bind(this));
         this.appendChild(tooltipIcon);
         console.log('The connectedCallback is invoked!')
     }
 
     _showTooltip() {
-        console.log('Event listner is working!')
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = this._tooltipText;
+        this.appendChild(this._tooltipContainer);
+    }
+
+    _destroyTooltip() {
+        this.removeChild(this._tooltipContainer);
     }
 }
 
