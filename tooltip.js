e00392c6bc53d2f0c21ffbbc70b0a35228b38d9f
liutkevych <oli@keepit.com>
2018-12-25 15:41:53 +0200

    Use html template

--- tooltip.js
+++ tooltip.js
@@ -4,25 +4,29 @@ class Tooltip extends HTMLElement {
         this._tooltipContainer;
         this._tooltipText = 'Some hurdcoded tooltip text!';
         this.attachShadow({mode: 'open'}); // Attach shadow DOM to custom element
+        const template = document.querySelector('#tooltip-template');
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
     }
 
-    // Only in this place we can place our element in DOM
+    // Only in this place we can place our element in  real DOM
     connectedCallback() {
         if (this.hasAttribute('text')){
             this._tooltipText = this.getAttribute('text');
         }
-        const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)';
+        const tooltipIcon = this.shadowRoot.querySelector('span');
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._destroyTooltip.bind(this));
         // Append new element to shadow DOM
         this.shadowRoot.appendChild(tooltipIcon); 
-        console.log('The connectedCallback is invoked!')
+        this.style.position = 'relative';
     }
 
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
         this._tooltipContainer.textContent = this._tooltipText;
+        this._tooltipContainer.style.background = 'grey';
+        this._tooltipContainer.style.position = 'absolute';
+        this._tooltipContainer.style.zIndex = '10';
         this.shadowRoot.appendChild(this._tooltipContainer);
     }
 
